be0dd54c2a3c3d6dee608c8662c7cd017407ef08
Anamaria Miranda <anny13miranda@gmail.com>
2024-03-30 14:52:06 -0500

    Added navigation bar and main's color page #4

--- src/app/core/index.tsx
+++ src/app/core/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import Router from '../Router';
+import LoggedOutLayout from '../layouts/LoggedOutLayout';
 
 const Core = () => {
 
   return (
     <BrowserRouter>
+      <LoggedOutLayout />
       <Router />
     </BrowserRouter>
   );
